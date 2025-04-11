15e518223fdd39d74c2a148ff958fea6668332dd
RLeclair <raulleclair@microsoft.com>
2025-04-11 09:50:37 -0700

    Generating observability client

--- rust/azure_iot_operations_services/gen.sh
+++ rust/azure_iot_operations_services/gen.sh
@@ -2,5 +2,9 @@
 ../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
  --clientOnly --modelFile ../../eng/dtdl/SchemaRegistry-1.json --sdkPath ../ --lang=rust --noProj \
  --outDir src/schema_registry/schemaregistry_gen
- 
+
+../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
+ --serverOnly --modelFile ../../eng/dtdl/akri-observability-metrics-operations.json --sdkPath ../ --lang=rust --noProj \
+ --outDir src/observability/observability_gen
+
 cargo fmt
