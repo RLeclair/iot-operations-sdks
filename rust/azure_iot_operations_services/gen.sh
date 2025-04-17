15be2a05c57ffafeb9232ed95934784ee514b623
olivakar <oliva.tanusree@gmail.com>
2025-04-17 13:43:12 -0700

    [Rust][Services] Device name generated files  (#705)

--- rust/azure_iot_operations_services/gen.sh
+++ rust/azure_iot_operations_services/gen.sh
@@ -2,5 +2,9 @@
 ../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
  --clientOnly --modelFile ../../eng/dtdl/SchemaRegistry-1.json --sdkPath ../ --lang=rust --noProj \
  --outDir src/schema_registry/schemaregistry_gen
- 
+
+../../codegen/src/Azure.Iot.Operations.ProtocolCompiler/bin/Debug/net9.0/Azure.Iot.Operations.ProtocolCompiler \
+ --clientOnly --modelFile ../../eng/dtdl/device-name-based-operations.json --sdkPath ../ --lang=rust --noProj \
+ --outDir src/azure_device_registry/device_name_gen
+
 cargo fmt
---
+++ rust/azure_iot_operations_services/src/azure_device_registry.rs
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+//! Types for Azure Device Registry operations.
+/// Azure Device Registry generated code
+mod device_name_gen;
