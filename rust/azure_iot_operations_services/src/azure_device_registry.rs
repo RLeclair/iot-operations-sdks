063800c9a8c28875657e5a788fe2efd2f7b84e96
vaavva <vaavva@microsoft.com>
2025-04-18 13:45:34 -0400

    scaffolding for adr types file

--- rust/azure_iot_operations_services/src/azure_device_registry.rs
+++ rust/azure_iot_operations_services/src/azure_device_registry.rs
@@ -1,6 +1,24 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+// imports section (TODO: remove this comment)
+
 //! Types for Azure Device Registry operations.
+
 /// Azure Device Registry generated code
 mod device_name_gen;
+
+// ~~~~~~~~~~~~~~~~~~~SDK Created Structs~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~Helper fns ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+fn option_vec_from<T, U>(source: Option<Vec<T>>, into_fn: impl Fn(T) -> U) -> Option<Vec<U>> {
+  source.map(|vec| vec.into_iter().map(into_fn).collect())
+}
+
+// ~~~~~~~~~~~~~~~~~~~Common DTDL Equivalent Structs~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~Device Endpoint DTDL Equivalent Structs~~~~
+
+// ~~~~~~~~~~~~~~~~~~~Asset DTDL Equivalent Structs~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~Asset Status DTDL Equivalent Structs~~~~~~~
