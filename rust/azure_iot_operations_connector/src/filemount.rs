6ffe0a142ff5950ffeca6df9d6368f1d06584b38
Raul Leclair <raulleclair@microsoft.com>
2025-04-24 15:41:35 -0700

    [Rust][Connector] File Mount ADR Client + Sample (#739)
    From #736 :
    - Simplified the API for observing an ADR file mount
    - Now a single observation for the file mount is created from which
    device endpoint observations are derived. Each device observation can in
    turn derive asset observations.
    - Removed unnecessary methods based on the above.
    - Added error handling for the API with a few exceptions (marked TODO)
    and errors surfacing at the watcher level.
    - Adds happy path testing for the API
    
    _Addressed comments in previous PR_
    
    This PR:
    - Adds filemount module which will eventually hold watcher
    functionality.
    - Adds an example showing sample usage.

---
+++ rust/azure_iot_operations_connector/src/filemount.rs
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+//! Contains structs and functions for interacting with a file mount and modules for utilities that
+//! use the file mount.
+
+pub mod azure_device_registry;
+
+// TODO: Add the file mount structs and functions here.
